46f5e86345b65073b708515a01943756c7d88780
AnnelynnTruyens <annelynn.truyens@student.arteveldehs.be>
2025-06-14 15:50:36 +0200

    App accessibility page

--- app/src/pages/General/Accessibility/Accessibility.tsx
+++ app/src/pages/General/Accessibility/Accessibility.tsx
@@ -3,6 +3,11 @@ const Accessibility = () => {
 		<main id="main" className="main">
 			<title>Accessibility Statement | Wheelable Hotels</title>
 			<h1>Accessibility statement</h1>
+			<p>
+				We aim to be as accessible as possible. Our accessibility screening for
+				the website is not finished yet. You will be able to find the digital
+				accessibility information here, once it's done.
+			</p>
 		</main>
 	);
 };
